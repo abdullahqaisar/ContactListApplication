0bcf6ee0c9dc33ad7037584d5aa65d847f1ad27d
abdullah <abdullahqaisarr@gmail.cm>
2022-07-01 22:39:41 +0500

    changed data and design

--- src/contacts.js
+++ src/contacts.js
@@ -1,27 +1,27 @@
 const contacts = [
   {
     id: 1,
-    name: "Beyonce",
+    name: "Abdullah",
     imgURL:
-      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
-    phone: "+123 456 789",
+      "https://hips.hearstapps.com/esq.h-cdn.co/assets/16/28/1468416014-rami-mr-robot.JPG",
+    phone: "+92 300 1123290",
     email: "b@beyonce.com"
   },
   {
     id: 2,
-    name: "Jack Bauer",
+    name: "Ali Khan",
     imgURL:
       "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
-    phone: "+987 654 321",
+    phone: "+92 310 6543289",
     email: "jack@nowhere.com"
   },
   {
     id: 3,
-    name: "Chuck Norris",
+    name: "John Cena",
     imgURL:
       "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
-    phone: "+918 372 574",
-    email: "gmail@chucknorris.com"
+    phone: "+122 435 532",
+    email: "Wonder why you can't see an image?"
   }
 ];
 
